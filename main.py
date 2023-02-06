0af2b0ac986884589676c62c24c081b5d3fdc148
Merholg <ixibitus@gmail.com>
2023-02-06 09:44:14 +0300

    Create Project

---
+++ main.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#
+#  Copyright 2022 mc <mc@PyBuntu>
+#
+# This is a sample Python script.
+
+
+import sys
+import os
+# import mimetypes
+from PIL import Image
+from pyzbar import pyzbar
+
+image_ext_list = list(
+    [".BMP", ".DDS", ".DIB", ".EPS", ".GIF", ".ICNS", ".ICO", ".IM", ".JPEG", ".JPG", ".MSP", ".PCX", ".PNG",
+     ".PPM", ".SGI", ".SPIDER", ".TGA", ".TIFF", ".WEBP", ".XBM"])
+
+
+def get_barcode(fullname):
+    barcodes = list()
+    if os.path.exists(fullname) and os.path.isfile(fullname):
+        filename = os.path.basename(fullname)
+        name, ext = os.path.splitext(filename)
+        try:
+            with Image.open(fullname) as im:
+                barcodes = pyzbar.decode(im)
+        except OSError as os_error:
+            print("Error open image as: {} because: {}".format(fullname, os_error))
+            return barcodes
+        except ValueError as val_error:
+            print("Format could not be determined from the file as: {} because: {}".format(fullname, val_error))
+            return barcodes
+    else:
+        print("No {} exist or it is no file".format(fullname))
+        return barcodes
+
+    return barcodes
+
+
+def get_walks(path):
+    """
+    Return list of image files from dir
+    :param path: maindir
+    :return: list of files in maindir
+    """
+    filelist = []
+    if os.path.exists(path) and os.path.isdir(path):
+        # print("Check {} ...".format(path)
+        for dirpath, dirnames, filenames in os.walk(path):
+            for f in filenames:
+                # image_file = os.path.join(dirpath, f)
+                # check_type = mimetypes.guess_type(image_file)[0]
+                # if not check_type is None:
+                #   print(check_type.split("/")[0])
+                #   if "image" == check_type.split("/")[0]:
+                name, ext = os.path.splitext(f)
+                if ext.upper() in image_ext_list:
+                    filelist.append(os.path.join(dirpath, f))
+    else:
+        print("No {} exist or it is no dir".format(path))
+
+    return filelist
+
+
+def main(args):
+    """
+    Convert all found images in setted dir and it`s subdir
+    :param args: set dir or empty if dir is current workdir
+    :return: 0 if no errors
+    """
+    if len(args) < 2:
+        path = os.getcwd()
+    else:
+        path = args[1]
+
+    if '~' in path:
+        fullpath = os.path.expanduser(path)
+    else:
+        fullpath = os.path.realpath(path)
+
+    file_list = get_walks(fullpath)
+    if len(file_list) > 0:
+        out_list = list()
+        for image_file in file_list:
+            dirname, filename = os.path.split(image_file)
+            relpath = dirname.replace(fullpath, "")
+            barcodes = get_barcode(image_file)
+            i = 0
+            if len(barcodes) > 0:
+                # print("File {} barcode Found".format(image_file))
+                for barcode in barcodes:
+                    # print("barcode as: {}".format(barcode.data))
+                    i += 1
+                    out_list.append("{}\t{}\t{}\t{}\n".format(filename, relpath.replace("/\\", ""), i,
+                                                              barcode.data.decode("utf-8")))
+            else:
+                # print("File {} barcode Not Found".format(image_file))
+                out_list.append("{}\t{}\t{}\t{}\n".format(filename, relpath.replace("/\\", ""), i, ""))
+        try:
+            with open('foundcodes.csv', 'w', encoding='utf-8') as outfile:
+                outfile.writelines(out_list)
+        except OSError as os_error:
+            print("Error operate with file as: {} because: {}".format('foundcodes.csv', os_error))
+    else:
+        print("No files found ...")
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
