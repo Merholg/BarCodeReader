338e08dbada74750773da60df154ee1897352eeb
Merholg <ixibitus@gmail.com>
2023-02-16 15:55:47 +0300

    BRResolver develop

--- BRResolver.py
+++ BRResolver.py
@@ -13,31 +13,41 @@ from pyzbar import pyzbar
 def get_barcode(args):
     barcodes = list()
     while True:
+        barcodes.clear()
         fullname = input()
         if os.path.exists(fullname) and os.path.isfile(fullname):
-            filename = os.path.basename(fullname)
-            name, ext = os.path.splitext(filename)
-            try:
-                with Image.open(fullname) as im:
-                    barcodes = pyzbar.decode(im)
-                    if not (len(barcodes) > 0):
-                        sharp_im = im.filter(ImageFilter.SHARPEN)
-                        barcodes = pyzbar.decode(sharp_im)
-            except OSError as os_error:
-                print("Error open image as: {} because: {}".format(fullname, os_error))
-                return barcodes
-            except ValueError as val_error:
-                print("Format could not be determined from the file as: {} because: {}".format(fullname, val_error))
-                return barcodes
-        # except:
-        #     print("Another error or warning: {}".format(fullname))
-        #     return barcodes
+            with Image.open(fullname) as im:
+                barcodes = pyzbar.decode(im)
+                if not (len(barcodes) > 0):
+                    sharp_im = im.filter(ImageFilter.SHARPEN)
+                    barcodes = pyzbar.decode(sharp_im)
+            i = 0
+            if len(barcodes) > 0:
+                # print("File {} barcode Found".format(image_file))
+                for barcode in barcodes:
+                    # print("barcode as: {}".format(barcode.data))
+                    i += 1
+                    print("{}\t{}\t{}\t{}\n".format(filename, relpath.replace("/\\", ""), i,
+                                                    barcode.data.decode("utf-8")))
+            else:
+                # print("File {} barcode Not Found".format(image_file))
+                print("{}\t{}\t{}\t{}\n".format(filename, relpath.replace("/\\", ""), i, ""))
         else:
             # print("No {} exist or it is no file".format(fullname))
-
+            print("")
 
     return
 
 
 if __name__ == '__main__':
-    sys.exit(get_barcode(sys.argv))
+    if len(sys.argv) < 2:
+        path = os.getcwd()
+    else:
+        path = sys.argv[1]
+
+    if '~' in path:
+        fullpath = os.path.expanduser(path)
+    else:
+        fullpath = os.path.realpath(path)
+
+    sys.exit(get_barcode(fullpath))
